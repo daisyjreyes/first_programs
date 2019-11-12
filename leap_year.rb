5039bc05695c33310b05aa559dc9f13a3336440f
Ubuntu <ubuntu@ip-172-31-89-252.ec2.internal>
2019-11-12 16:30:08 +0000

    leap_year.rb

---
+++ leap_year.rb
@@ -0,0 +1,9 @@
+puts "Enter a starting year"
+starting_year = gets.chomp
+puts "Enter a ending_year"
+ending_year = gets.chomp
+i = starting_year
+i == % 400 && % 100 && % 4 
+a = ending_year
+a == % 400 && % 100 && % 4
+end 
