b74baaafa25f7ddd8b22cc3b8c2d513d8c24441e
Ubuntu <ubuntu@ip-172-31-89-252.ec2.internal>
2019-11-12 07:03:35 +0000

    sorted_words.rb

---
+++ my_programs.rb
@@ -0,0 +1,8 @@
+class sample
+    def hello
+      puts "Hello, world"
+    end 
+end 
+
+s = Sample.new
+s.hello
--- practice.rb
+++ practice.rb
@@ -1,11 +1,7 @@
-puts 'What is your full name?'
-name = gets.chomp
-puts 'did you know there are ' + name.length.to_s + ' characters in your name, ' + name + '?'
-__END__
-letters = "aAbBcCdDeE"
-puts letters.upcase
-puts letters.downcase
-puts letters.swapcase
-puts letters.capitalize
-puts 'a'.capitalize
-puts letters
+sum = 0
+3.times do
+    pring "Enter Number: "
+    sum = sum + gets.chomp.to_f
+end
+avg = sum/3
+puts "Average: #{avg}"
